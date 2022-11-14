a57076e8b7ac06fea100997a6a1a0158c9bca6d5
bassima_gh <ghba20wu@stident.ju.se>
2022-11-14 17:35:15 +0100

    fixed the responsiveness for the second section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,13 @@ tailwind.config = {
 			fontFamily: {
 				'anton': ['Anton', 'sans-serif'] 
 			},
+			screens: {
+				'md': '903px',
+			},
 		}
 	},
 	screens: {
 		'sm': '640px',
 		// => @media (min-width: 640px) { ... }
-
-		'md': '868px',
-		// => @media (min-width: 768px) { ... }
-
-		'lg': '1024px',
-		// => @media (min-width: 1024px) { ... }
-
-		'xl': '1280px',
-		// => @media (min-width: 1280px) { ... }
-
-		'2xl': '1536px',
-		// => @media (min-width: 1536px) { ... }
-	},
+	}
 }
